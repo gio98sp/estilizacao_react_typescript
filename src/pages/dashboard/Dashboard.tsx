40b97360a0cbac304f740ad168888058b074d831
Giovane Santos <giovane98sp@gmail.com>
2023-08-03 18:35:37 -0300

    add css in js

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,22 @@
-import './Dashboard.css'
+import { createUseStyles } from 'react-jss'
+
+const useClasses = createUseStyles({
+  paragraph: {
+    backgroundColor: 'black',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: 'red'
+    },
+    '&:active': {
+      backgroundColor: 'blue'
+    }
+  }  
+})
 
 export const Dashboard = () => {
+  const classes = useClasses()
 
   return (
-    <p className='paragraph'>Dashboard</p>
+    <p className={classes.paragraph}>Dashboard</p>
   )
 }
